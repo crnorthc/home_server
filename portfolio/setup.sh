fdda824dd81780b275e5119b2ce4725370697eed
Caleb Northcott <crnorthc99@gmail.com>
2025-06-16 19:24:39 -0400

    fix build

--- portfolio/setup.sh
+++ portfolio/setup.sh
@@ -3,4 +3,4 @@
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
 USER_NAME=$(whoami)
 
-sudo cp -r /home/$USER_NAME/dist/* /var/www/html
+cp -r /home/$USER_NAME/dist/* /var/www/html
