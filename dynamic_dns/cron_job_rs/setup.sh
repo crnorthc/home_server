5a9bdcfe4d4c5024ed4e78148820793c986bb098
Caleb Northcott <crnorthc99@gmail.com>
2025-06-25 19:25:39 -0400

    fix build

--- dynamic_dns/cron_job_rs/setup.sh
+++ dynamic_dns/cron_job_rs/setup.sh
@@ -7,5 +7,5 @@ cd /home/$USER_NAME/cron_job_rs
 
 cargo build --release
 
-echo "*/5 * * * * PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin cd /home/$USER_NAME/cron_job_rs && ./target/release/cron_job_rs > /var/log/ip_update.log 2>&1" >> /home/$USER_NAME/cron_job/crontab
+echo "*/5 * * * * PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin cd /home/$USER_NAME/cron_job_rs && ./target/release/cron_job_rs > /var/log/ip_update.log 2>&1" >> /home/$USER_NAME/cron_job_rs/crontab
 crontab -u $USER_NAME crontab
